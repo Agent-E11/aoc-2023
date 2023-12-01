4f5830c87c85d8bfbbadf179681578cd372e5872
Eamon <eamon.r.burns@gmail.com>
2023-12-01 08:37:44 -0800

    - Initial commit -

---
+++ rust_solutions/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn day_1(input: String) -> u32 {
+    todo!();
+}
---
+++ rust_solutions/src/main.rs
@@ -0,0 +1,5 @@
+use rust_solutions::day_1;
+fn main() {
+    let day_1_in = include_str!("../../inputs/day-1.txt");
+    println!("{}", day_1_in);
+}
