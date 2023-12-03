08a5ce00182aa61c0713d9ad37046be563cc2ad0
Eamon <eamon.r.burns@gmail.com>
2023-12-02 19:37:01 -0800

    - Day 2 part 1 and 2 -

--- rust_solutions/src/lib.rs
+++ rust_solutions/src/lib.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+// ----- Day 1 -----
+
 pub fn day1_pt1(input: &str) -> u32 {
 
     let mut nums = Vec::new();
@@ -24,4 +28,69 @@ pub fn day1_pt1(input: &str) -> u32 {
     }
 
     nums.iter().sum()
+}
+
+// ----- Day 2 -----
+
+type Game = (usize, Vec<(usize, usize, usize)>);
+
+pub fn parse_games(input: &str) -> Result<Vec<Game>, String> {
+
+    let color_index_map: HashMap<String, usize> = HashMap::from([
+        ("red".to_owned(), 0),
+        ("green".to_owned(), 1),
+        ("blue".to_owned(), 2),
+    ]);
+
+    let mut parsed_games: Vec<Game> = Vec::new();
+
+    for line in input.lines() {
+        let (idx_str, cube_sets_str) = line.split_once(':').unwrap();
+        let game_idx: usize = idx_str.split_once(' ').unwrap().1.parse().unwrap();
+        let cube_sets: Vec<(usize, usize, usize)> = cube_sets_str.split(';').map(|set| {
+            println!("Set: {set}");
+            let collections = set.splitn(3, ',');
+
+            let mut set_array = [0, 0, 0];
+
+            for collection in collections {
+                let (num, color) = collection.trim().split_once(' ').unwrap();
+                println!("Number: {num}, Color: {color}");
+                let idx = color_index_map[&color.to_lowercase()];
+                set_array[idx] = num.parse().unwrap();
+            }
+            set_array.into()
+        }).collect();
+
+        parsed_games.push((game_idx, cube_sets));
+    }
+
+    Ok(parsed_games)
+}
+
+pub fn filter_limits(games: Vec<Game>, limits: (usize, usize, usize)) -> Vec<usize> {
+    let (red_max, green_max, blue_max) = limits;
+    let mut game_ids = Vec::new();
+    for game in games {
+        let mut include_game = true;
+        for (r, g, b) in game.1 {
+            if r>red_max || g>green_max || b>blue_max {
+                include_game = false;
+            }
+        }
+        if include_game { game_ids.push(game.0) }
+    }
+    game_ids
+}
+
+pub fn find_min(games: Vec<Game>) -> Vec<(usize, usize, usize)> {
+    games.iter().map(|game| {
+        let mut maxes = (0, 0, 0);
+        for collection in &game.1 {
+            if collection.0 > maxes.0 { maxes.0 = collection.0 }
+            if collection.1 > maxes.1 { maxes.1 = collection.1 }
+            if collection.2 > maxes.2 { maxes.2 = collection.2 }
+        }
+        maxes
+    }).collect()
 }
--- rust_solutions/src/main.rs
+++ rust_solutions/src/main.rs
@@ -1,5 +1,22 @@
-use rust_solutions::day1_pt1;
+use rust_solutions::{parse_games, filter_limits, find_min};
 fn main() {
-    let day_1_in = include_str!("../../inputs/day-1.txt");
-    println!("{}", day1_pt1(day_1_in));
+    let day_2_in = include_str!("../../inputs/day-2.txt");
+    let games = parse_games(day_2_in).unwrap();
+    println!("Games: {:?}", games);
+
+    let minimums = find_min(games);
+    println!("Minimums: {:?}", minimums);
+
+    let powers = minimums.iter().map(|mins| {
+        mins.0 * mins.1 * mins.2
+    });
+
+    let res: usize = powers.sum();
+
+    println!("Result: {}", res);
+
+    // let limits = (12, 13, 14);
+    // let game_ids = filter_limits(games, limits);
+    // println!("Filtered: {game_ids:?}\nLimits: {limits:?}");
+    // println!("Sum: {}", game_ids.iter().sum::<usize>());
 }
