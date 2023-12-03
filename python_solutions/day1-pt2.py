114157ee6bf618d9ea2b63293de3172a0df0a54e
Eamon <eamon.b.school@gmail.com>
2023-12-02 19:39:12 -0800

    - Added `python_solutions` directory -

---
+++ python_solutions/day1-pt2.py
@@ -0,0 +1,21 @@
+input = open('../inputs/day-1.txt', 'r').read()
+
+# lines = input.splitlines()
+
+# print(lines)
+
+input_output = {
+    "two1nine": 29,
+    "eightwothree": 83,
+    "abcone2threexyz": 13,
+    "xtwone3four": 24,
+    "4nineeightseven2": 42,
+    "zoneight234": 14,
+    "7pqrstsixteen": 76,
+}
+
+lines = list(input_output.keys())
+
+print(lines)
+
+
